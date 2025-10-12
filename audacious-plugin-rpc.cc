9f476b8157d104d3aa517be1bd0359b8f34ed6aa
onegen <84882649+onegentig@users.noreply.github.com>
2025-10-12 17:24:58 +0000

    feat: add setting to clear RPC when paused

--- audacious-plugin-rpc.cc
+++ audacious-plugin-rpc.cc
@@ -17,7 +17,7 @@
 #define APPLICATION_ID "484736379171897344"
 
 static bool is_connected = false; // Guard, just in case
-static const char *SETTING_EXTRA_TEXT = "extra_text";
+static bool hide_when_paused = false;
 
 class RPCPlugin : public GeneralPlugin {
    public:
@@ -62,7 +62,13 @@ void cleanup_discord() {
 
 void update_presence() {
      if (!is_connected) return;
-     rpc.setPresence(presence).refresh();
+     rpc
+          .clearPresence()
+          .refresh(); // TODO: Timestamps are very unreliably cleared.
+                      // This helps a little, but it’s still very wonky.
+     rpc
+          .setPresence(presence)
+          .refresh();
 }
 
 void init_presence() {
@@ -91,8 +97,12 @@ void title_changed() {
      }
 
      const bool playing = !aud_drct_get_paused();
-     const Tuple tuple = aud_drct_get_tuple();
+     if (hide_when_paused && !playing) {
+          clear_discord();
+          return;
+     }
 
+     const Tuple tuple = aud_drct_get_tuple();
      std::string title(tuple.get_str(Tuple::Title));
      std::string artist(tuple.get_str(Tuple::Artist));
      std::string album(tuple.get_str(Tuple::Album));
@@ -111,8 +121,8 @@ void title_changed() {
 
      if (playing) {
           const auto clock = std::chrono::system_clock::now();
-          const int64_t now_s = std::chrono::duration_cast<std::chrono::seconds>(clock.time_since_epoch()).count();
-          int64_t start_ts = now_s - (aud_drct_get_time() / 1000);
+          const int64_t now_ts = std::chrono::duration_cast<std::chrono::seconds>(clock.time_since_epoch()).count();
+          int64_t start_ts = now_ts - (aud_drct_get_time() / 1000);
           presence.setStartTimestamp(start_ts);
           if (tuple.get_value_type(Tuple::Length) == Tuple::Int && tuple.get_int(Tuple::Length) > 0)
                presence.setEndTimestamp(start_ts + (tuple.get_int(Tuple::Length) / 1000));
@@ -152,16 +162,17 @@ void RPCPlugin::cleanup() {
 }
 
 const char RPCPlugin::about[] = N_(
-    "Discord Rich Presence (RPC) playing status plugin\n\n"
-    "(c) onegen <onegen@onegen.dev> (2024–2025)\n"
-    "(c) Derzsi Dániel <daniel@tohka.us> et al. (2018–2022)\n"
-    "https://github.com/onegentig/audacious-discord-rpc"
+    "Discord Rich Presence (RPC) playing status plugin\n"
+    "https://github.com/onegentig/audacious-discord-rpc\n"
+    " © onegen <onegen@onegen.dev> (2024–2025)\n"
+    " © Derzsi Dániel <daniel@tohka.us> et al. (2018–2022)\n\n"
+    "Displays the current playing track as your Discord status.\n"
+    "(Discord should be running before this plugin is loaded.)"
 );
 
-const PreferencesWidget RPCPlugin::widgets[]
-    = {WidgetEntry(N_("Extra status text:"),
-                   WidgetString("audacious-plugin-rpc", SETTING_EXTRA_TEXT,
-                                title_changed)),
-       WidgetButton(N_("Fork on GitHub"), {open_github})};
+const PreferencesWidget RPCPlugin::widgets[] = {
+     WidgetCheck(N_("Hide presence when paused"), WidgetBool(hide_when_paused, title_changed)),
+     WidgetButton(N_("Show on GitHub"), {open_github})
+};
 
 const PluginPreferences RPCPlugin::prefs = {{widgets}};
